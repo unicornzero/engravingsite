55611b9aadfc64b40cb2aba259ef35a1200f7a68
unicornzero <rylee.k@gmail.com>
2013-07-05 23:15:45 -0600

    secure_token updated and moved to appliation.yml file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Engravingsite::Application.config.secret_key_base = 'ed059ba8c9c0cc2c68d25bec251e0879e7a534e6a640b263f30ca67cb9798bea538a14afef002db800efcd90f7147956048a64acd7dde280a658ac9a038768bf'
+Engravingsite::Application.config.secret_key_base = CONFIG[:secret_token]
