ad18d3eb11a088df6e85b462bb933c706c8d390a
unicornzero <rylee.k@gmail.com>
2013-07-06 11:07:10 -0600

    refactored flickr user lookup

--- app/controllers/flickr_stream_controller.rb
+++ app/controllers/flickr_stream_controller.rb
@@ -1,14 +1,18 @@
 class FlickrStreamController < ApplicationController
 
-require 'flickraw'
-
+	require 'flickraw'
+  FlickRaw.api_key= CONFIG[:flickraw_api]
+  FlickRaw.shared_secret= CONFIG[:flickraw_secret]
 
   def main_stream
-    FlickRaw.api_key= CONFIG[:flickraw_api]
-    FlickRaw.shared_secret= CONFIG[:flickraw_secret]
-    @fname = CONFIG[:my_flickr_user]
-    @fuser = flickr.people.findByUsername(:username => @fname)
-    @fuser_hash= @fuser.to_hash
+  	user_lookup
     @fuser_name = @fuser_hash["username"]
   end
+
+  private
+  	def user_lookup
+	    @fname = CONFIG[:my_flickr_user]
+	    @fuser = flickr.people.findByUsername(:username => @fname)
+	    @fuser_hash= @fuser.to_hash
+  	end
 end
