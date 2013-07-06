13a043cebaed3eaf868f12ad2a9f187fb0da0c82
unicornzero <rylee.k@gmail.com>
2013-07-06 11:02:37 -0600

    display Flickr username

--- app/controllers/flickr_stream_controller.rb
+++ app/controllers/flickr_stream_controller.rb
@@ -1,11 +1,14 @@
 class FlickrStreamController < ApplicationController
 
 require 'flickraw'
-FlickRaw.api_key= CONFIG[:flickraw_api]
-FlickRaw.shared_secret= CONFIG[:flickraw_secret]
+
 
   def main_stream
-    @info = flickr.photos.getInfo(:photo_id => "3839885270")
-    @url = FlickRaw.url_photostream(@info) # => "http://www.flickr.com/photos/41650587@N02/"
+    FlickRaw.api_key= CONFIG[:flickraw_api]
+    FlickRaw.shared_secret= CONFIG[:flickraw_secret]
+    @fname = CONFIG[:my_flickr_user]
+    @fuser = flickr.people.findByUsername(:username => @fname)
+    @fuser_hash= @fuser.to_hash
+    @fuser_name = @fuser_hash["username"]
   end
 end
