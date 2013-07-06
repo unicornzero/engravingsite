35fa287217961f51df22721a5c69d94625f70b1c
unicornzero <rylee.k@gmail.com>
2013-07-05 23:20:31 -0600

    set root to: "static_pages#home"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Engravingsite::Application.routes.draw do
+  root to: "static_pages#home"
+
   get "/home", to: "static_pages#home", as: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
