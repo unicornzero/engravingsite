efedd17a46fee2a69eb535443314c7fd40a6dd35
unicornzero <rylee.k@gmail.com>
2013-07-05 20:58:28 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Engravingsite::Application.config.secret_key_base = 'ed059ba8c9c0cc2c68d25bec251e0879e7a534e6a640b263f30ca67cb9798bea538a14afef002db800efcd90f7147956048a64acd7dde280a658ac9a038768bf'
