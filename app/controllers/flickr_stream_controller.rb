6d30307236a8c0fe1950e0ca58d49a3158df2622
unicornzero <rylee.k@gmail.com>
2013-07-18 14:47:21 -0600

    refactored flickrapi connection

--- app/controllers/flickr_stream_controller.rb
+++ app/controllers/flickr_stream_controller.rb
@@ -1,9 +1,5 @@
 class FlickrStreamController < ApplicationController
 
-  #helper_method :pics_in_set
-  #before_filter :photoset_list, only: :main_stream
-
-
   def main_stream
     @flickr = MyFlickrAPI.new
     @flickr.load_main_stream
@@ -15,24 +11,5 @@ class FlickrStreamController < ApplicationController
     @flickr.find_all_photos
     @all_photos = @flickr.all_photos
   end
-
-=begin
-  require 'flickraw'
-  FlickRaw.api_key= CONFIG[:flickraw_api]
-  FlickRaw.shared_secret= CONFIG[:flickraw_secret]
-
-
-  def photoset_list
-    @fid = CONFIG[:my_flickr_id]
-    @psets = flickr.photosets.getList(user_id: @fid).to_hash["photoset"]
-    @psets_hash = {}
-    @psets.each { |s| 
-      @psets_hash[s["id"]] = s["title"] }
-  end
-
-  def pics_in_set(set_id)
-    @pics_response = flickr.photosets.getPhotos(photoset_id: set_id, privacy_filter: "1")
-  end
-=end
 
 end
--- app/services/my_flickr_api.rb
+++ app/services/my_flickr_api.rb
@@ -1,7 +1,7 @@
 class MyFlickrAPI
 
   require 'flickraw'
-  attr_reader :psets, :psets_hash, :sets_of_photos, :all_photos
+  attr_reader :sets_of_photos, :all_photos
 
   def initialize
     FlickRaw.api_key = CONFIG[:flickraw_api]
@@ -17,28 +17,24 @@ class MyFlickrAPI
     @all_photos = flickr.people.getPublicPhotos(user_id: CONFIG[:my_flickr_id]).to_hash
   end
 
-  private
-
-  def find_photosets
-    @psets = flickr.photosets.getList(user_id: CONFIG[:my_flickr_id]).to_hash["photoset"]
-    #@psets_hash = {}
-    #@psets.each { |s| 
-    #  @psets_hash[s["id"]] = s["title"] }
-  end
-
-  def photo_list
-    @sets_of_photos = @psets.map do |set|
-      { 
-        id: set["id"],
-        title: set["title"],
-        photos: photos_in_set(set["id"])
-      }
+    private
+    def find_photosets
+      @psets = flickr.photosets.getList(user_id: CONFIG[:my_flickr_id]).to_hash["photoset"]
     end
-  end
 
-  def photos_in_set(set_id)
-    #@pics_response = flickr.photosets.getPhotos(photoset_id: set_id, privacy_filter: "1")
-    @pics_response = flickr.photosets.getPhotos(photoset_id: set_id, privacy_filter: "1")["photo"]
-  end
+    def photo_list
+      @sets_of_photos = @psets.map do |set|
+        { 
+          id: set["id"],
+          title: set["title"],
+          photos: photos_in_set(set["id"])
+        }
+      end
+    end
+
+    def photos_in_set(set_id)
+      #@pics_response = flickr.photosets.getPhotos(photoset_id: set_id, privacy_filter: "1")
+      @pics_response = flickr.photosets.getPhotos(photoset_id: set_id, privacy_filter: "1")["photo"]
+    end
 
 end
